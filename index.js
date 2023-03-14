1ac66d02735fb15434093b8c6f73f820e3085635
LincolnMuraguri <lincolnmuraguri93@gmail.com>
2023-03-14 09:03:24 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(Feet) {
+  if (Feet < 400) {
+    return "This one is on me!";
+  } else if (Feet > 2000 && Feet < 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else if (Feet > 2500) {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === 'NYC'?"Ok, sounds good.":"No go."
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tipAmount){
+  let response;
+    switch(tipAmount){
+    case "generous":
+    response= "Thank you so much.";
+    break;
+    case "not as generous":
+    response= "Thank you.";
+    break;
+    default: "Thanks for everything.";
+    response="Bye.";
+    break;}
+    return response;
+    }
